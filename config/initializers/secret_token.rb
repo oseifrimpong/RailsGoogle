e1dde2da60dba6b10ad4df9a0946619182088e1e
Obed Osei Frimpong <obedosei@gmail.com>
2015-08-03 12:50:55 +0800

    all

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AdwordsOnRails::Application.config.secret_token = 'b20bd3d29512f3f742e244b70bea74f9e11822c7c3c3ba512ea85b497e42901b863562a792d8394acd10c4787792552e4c4111f93e1cf7b3110bec9c0ce91153'
+AdwordsOnRails::Application.config.secret_token = 'bcdea5bcb25f5d3a83907304f69d1e2f3354d7af8032843edf88f873a2bf1ab5c400bf03f6b24a503c579afea104cf9178d38bdb2fc60308763bd4ee1eae3bc2'
